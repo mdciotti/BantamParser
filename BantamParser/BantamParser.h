53819a0ea3b032b708df7813ff101ed0d0d2322b
Maxwell Ciotti <max@mdc.io>
2015-08-18 02:41:01 -0500

    Initial Commit

---
+++ BantamParser/BantamParser.h
@@ -0,0 +1,19 @@
+//
+//  BantamParser.h
+//  BantamParser
+//
+//  Created by Maxwell Ciotti on 2015-08-18.
+//  Copyright © 2015 mdciotti. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BantamParser.
+FOUNDATION_EXPORT double BantamParserVersionNumber;
+
+//! Project version string for BantamParser.
+FOUNDATION_EXPORT const unsigned char BantamParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BantamParser/PublicHeader.h>
+
+
